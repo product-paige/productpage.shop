28a80132f62323f48881b8d51536996eddfd3d86
Paige Harris <hello@productpaige.com>
2025-12-11 04:04:35 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,7 +1,6 @@
 import Dashboard from './pages/Dashboard';
 import Shop from './pages/Shop';
 import Landing from './pages/Landing';
-import PublicShop from './pages/PublicShop';
 import Settings from './pages/Settings';
 import __Layout from './Layout.jsx';
 
@@ -10,7 +9,6 @@ export const PAGES = {
     "Dashboard": Dashboard,
     "Shop": Shop,
     "Landing": Landing,
-    "PublicShop": PublicShop,
     "Settings": Settings,
 }
 
